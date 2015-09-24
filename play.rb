b86bcdc7637e980ccf5f51adfac21dea2399396e
James Dabbs <jamesdabbs@gmail.com>
2015-09-24 10:43:12 -0400

    Extract player class

--- play.rb
+++ play.rb
@@ -1,14 +1,17 @@
 require "./game"
+require "./player"
 
 puts "What max do you want to play to?"
 limit = gets.chomp.to_i
 
 g = Game.new max: limit
+p = Player.new
 
 loop do
   puts "You have #{g.guesses_left} guesses"
   puts "Guess a number between #{g.min} and #{g.max}"
-  guess = gets.chomp.to_i
+  guess = p.get_guess
+
   result = g.check_guess guess
   if result == "correct"
     puts "You won!"
---
+++ player.rb
@@ -0,0 +1,5 @@
+class Player
+  def get_guess
+    gets.chomp.to_i
+  end
+end
